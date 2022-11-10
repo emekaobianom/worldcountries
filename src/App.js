5b0b1d1fb43f757f73db141b00f785d7d029e596
Emeka Obianom <3mekslondon@gmail.com>
2022-11-10 16:37:53 +0100

    can fetch countries using api

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 const App = () => {
+  //keep countries ;
+  const [countries, setCountries] = useState([]);
+
+  //get countries on load
+  useEffect(() => {
+    fetch("https://restcountries.com/v3.1/all?fields=name,cca2")
+      .then((response) => response.json())
+      .then((response) => {
+        setCountries(response);
+        console.log(response);
+      })
+      .catch((err) => console.error(err));
+  }, []);
+
   return (
     <>
       <h1>World Countries</h1>
-
       <hr />
+      {/* search box */}
+      {/* per page */}
+      <hr />
+      {/* List of Countries */}
+      {countries.map((country) => (
+        <li key={country.name.common}>{country.name.common}</li>
+      ))}
     </>
   );
 };
